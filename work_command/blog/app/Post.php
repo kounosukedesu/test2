7f00692d893dbeda5f5fd669e89c2547371bf90e
kounosukedesu <kounosuk0731@ezweb.ne.jp>
2022-03-15 14:54:57 +0900

    投稿の論理削除

--- work_command/blog/app/Post.php
+++ work_command/blog/app/Post.php
@@ -3,19 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use SoftDeletes;
      // $post->fill($input)で受け取るキーを指定
     // 今回のキーはcreate.blade.phpのname=Post[]
     protected $fillable = [
     'title',
     'body',
-];
+    ];
     public function getPaginateByLimit(int $limit_count = 5)
-{
-    // updated_atで降順に並べたあと、limitで件数制限をかける
-    return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
-}
+    {
+        // updated_atで降順に並べたあと、limitで件数制限をかける
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
 
 }
